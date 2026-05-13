4e7841c2ff8b29cf457445e8562b2ad5b89a2125
AtiqurRahmanAtik <atikahmedtanvir84@gmail.com>
2026-05-13 14:02:53 +0600

    added

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS
   : [
       "http://localhost:5173",
       "http://localhost:3000",
-      "https://your-react-frontend-url.vercel.app" // <-- UPDATE THIS
+      "https://school-manage-ment-back-endd.vercel.app"
     ];
 
 app.use(
